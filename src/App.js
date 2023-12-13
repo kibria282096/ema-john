ba8e9045d802feaaa4704fa0cf629e96db986f28
kibria282096 <jucse282096@gmail.com>
2023-12-13 20:25:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Main from "./components/Layout/Main";
+import About from "./components/About/About";
+import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
+import { productsandCardloader } from "./loaders/productsandCardloader";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          loader: () => fetch("products.json"),
+          element: <Shop></Shop>,
+        },
+        {
+          path: "/orders",
+          loader: productsandCardloader,
+          element: <Orders></Orders>,
+        },
+        {
+          path: "/inventory",
+          element: <Inventory></Inventory>,
+        },
+      ],
+    },
+    {
+      path: "/about",
+      element: <About></About>,
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
